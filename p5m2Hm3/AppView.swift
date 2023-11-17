7cff5f2d8f1fc3dcfb1f0a496b91f968f4d412f4
Karen Uzunyan <klaatuka82@gmail.com>
2023-11-17 13:08:02 +0300

    reOgrCode

--- p5m2Hm3/AppView.swift
+++ p5m2Hm3/AppView.swift
@@ -1,8 +1,27 @@
-//
-//  AppView.swift
-//  p5m2Hm3
-//
-//  Created by Карен Узунян on 17.11.2023.
-//
+
 
 import Foundation
+import UIKit
+
+class AppView{
+    
+    func getLabel(text: String, fontSize: CGFloat = 12, weight: UIFont.Weight = .regular, color: UIColor = .appBlack) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: fontSize, weight: weight)
+        label.textColor = color
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+    
+    func getImage(name: UIImage) -> UIImageView {
+        let img = UIImageView()
+        img.image = name
+        img.contentMode = .scaleAspectFit
+        img.layer.cornerRadius = 40
+//        img.layer.cornerRadius = img.frame.size.width / 2
+        img.clipsToBounds = true
+        img.translatesAutoresizingMaskIntoConstraints = false
+        return img
+    }
+}
--- p5m2Hm3/ViewController.swift
+++ p5m2Hm3/ViewController.swift
@@ -4,34 +4,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var avatarImg: UIImageView = {
-        let img = UIImageView()
-        img.image = UIImage(named: "Image-80")
-        img.contentMode = .scaleAspectFit
-        img.layer.cornerRadius = 40
-//        img.layer.cornerRadius = img.frame.size.width / 2
-        img.clipsToBounds = true
-        img.translatesAutoresizingMaskIntoConstraints = false
-        return img
-    }()
-    
-    lazy var nameAcountOwner: UILabel = {
-        let name = UILabel()
-        name.text = "Имя Фамилия"
-        name.font = .boldSystemFont(ofSize: 20)
-        name.textColor = .black
-        name.translatesAutoresizingMaskIntoConstraints = false
-        return name
-    }()
-    
-    lazy var aboutAcountOwner: UILabel = {
-        let name = UILabel()
-        name.text = "О себе"
-        name.font = .boldSystemFont(ofSize: 12)
-        name.textColor = .black
-        name.translatesAutoresizingMaskIntoConstraints = false
-        return name
-    }()
+    private var appView = AppView()
     
     lazy var viewAbout: UIView = {
         let view = UIView()
@@ -39,51 +12,6 @@ class ViewController: UIViewController {
         view.layer.cornerRadius = 14
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
-    }()
-    
-    lazy var textAbout: UILabel = {
-        let name = UILabel()
-        name.text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
-        name.numberOfLines = 0
-        name.font = .systemFont(ofSize: 12)
-        name.textColor = .black
-        name.translatesAutoresizingMaskIntoConstraints = false
-        return name
-    }()
-    
-    lazy var photoAcountOwner: UILabel = {
-        let name = UILabel()
-        name.text = "Фото"
-        name.font = .boldSystemFont(ofSize: 12)
-        name.textColor = .black
-        name.translatesAutoresizingMaskIntoConstraints = false
-        return name
-    }()
-    
-    lazy var photoStack: UIStackView = {
-        let stack = UIStackView()
-        stack.distribution = .fillEqually
-        return stack
-    }()
-    
-    lazy var leftImg: UIImageView = {
-        let img = UIImageView()
-        img.image = UIImage(named: "Rectangle 2")
-        img.contentMode = .scaleAspectFit
-        img.layer.cornerRadius = 20
-        img.clipsToBounds = true
-        img.translatesAutoresizingMaskIntoConstraints = false
-        return img
-    }()
-    
-    lazy var rightImg: UIImageView = {
-        let img = UIImageView()
-        img.image = UIImage(named: "Rectangle 3")
-        img.contentMode = .scaleAspectFit
-        img.layer.cornerRadius = 20
-        img.clipsToBounds = true
-        img.translatesAutoresizingMaskIntoConstraints = false
-        return img
     }()
     
     lazy var editBtn: UIButton = {
@@ -107,10 +35,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         setView()
+        
     }
     
     func setView(){
-        
+        // фото аватара
+        let avatarImg = appView.getImage(name: UIImage(named: "Image-80")!)
         view.addSubview(avatarImg)
         NSLayoutConstraint.activate([
             avatarImg.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -118,13 +48,15 @@ class ViewController: UIViewController {
             avatarImg.widthAnchor.constraint(equalToConstant: 80),
             avatarImg.heightAnchor.constraint(equalToConstant: 80)
         ])
-        
+        // имя владельца аккаунта
+        let nameAcountOwner = appView.getLabel(text: "Имя Фамилия", fontSize: 20, weight: .black)
         view.addSubview(nameAcountOwner)
         NSLayoutConstraint.activate([
             nameAcountOwner.topAnchor.constraint(equalTo: avatarImg.bottomAnchor, constant: 19),
             nameAcountOwner.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
         // текст - О себе
+        let aboutAcountOwner = appView.getLabel(text: "О себе", weight: .bold)
         view.addSubview(aboutAcountOwner)
         NSLayoutConstraint.activate([
             aboutAcountOwner.topAnchor.constraint(equalTo: nameAcountOwner.bottomAnchor, constant: 39),
@@ -140,6 +72,8 @@ class ViewController: UIViewController {
         ])
         
         // view куда вложен текст - о себе( содержание )
+        let textAbout = appView.getLabel(text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo")
+        textAbout.numberOfLines = 0
         viewAbout.addSubview(textAbout)
         NSLayoutConstraint.activate([
             textAbout.topAnchor.constraint(equalTo: viewAbout.topAnchor, constant: 20),
@@ -148,44 +82,30 @@ class ViewController: UIViewController {
             textAbout.bottomAnchor.constraint(equalTo: viewAbout.bottomAnchor, constant: -20)
         ])
         // текст - ФОТО
+        let photoAcountOwner = appView.getLabel(text: "Фото", weight: .bold)
         view.addSubview(photoAcountOwner)
         NSLayoutConstraint.activate([
             photoAcountOwner.topAnchor.constraint(equalTo: viewAbout.bottomAnchor, constant: 53),
             photoAcountOwner.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 41)
         ])
+        let leftImg = appView.getImage(name: UIImage(named: "Rectangle 2")!)
+        leftImg.layer.cornerRadius = 20
         
-        // 2 фотографии без стека
-        
-//        view.addSubview(photoStack)
-//        view.addSubview(leftImg)
-//        view.addSubview(rightImg)
-//        NSLayoutConstraint.activate([
-//            leftImg.heightAnchor.constraint(equalToConstant: 170),
-//            leftImg.widthAnchor.constraint(equalToConstant: 170),
-//            rightImg.heightAnchor.constraint(equalToConstant: 170),
-//            rightImg.widthAnchor.constraint(equalToConstant: 170),
-//            
-//            leftImg.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            leftImg.topAnchor.constraint(equalTo: photoAcountOwner.bottomAnchor, constant: 14),
-//            rightImg.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            rightImg.topAnchor.constraint(equalTo: photoAcountOwner.bottomAnchor, constant: 14)
-//        ])
-        
-        // 2 фотографии в стеке constraint !!!!!!!!!!!!!!!!!!!!!!
+        let rightImg = appView.getImage(name: UIImage(named: "Rectangle 3")!)
+        rightImg.layer.cornerRadius = 20
         
         view.addSubview(stackPhoto)
         stackPhoto.addArrangedSubview(leftImg)
         stackPhoto.addArrangedSubview(rightImg)
         
         NSLayoutConstraint.activate([
-
-        leftImg.heightAnchor.constraint(equalTo: leftImg.widthAnchor),
-        
-        rightImg.heightAnchor.constraint(equalTo: rightImg.widthAnchor),
-        
-        stackPhoto.topAnchor.constraint(equalTo: photoAcountOwner.bottomAnchor, constant: 20),
-        stackPhoto.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        stackPhoto.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            
+            leftImg.heightAnchor.constraint(equalTo: leftImg.widthAnchor),
+            rightImg.heightAnchor.constraint(equalTo: rightImg.widthAnchor),
+            
+            stackPhoto.topAnchor.constraint(equalTo: photoAcountOwner.bottomAnchor, constant: 20),
+            stackPhoto.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackPhoto.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
         
         // кнопка прикреплена cнизу основного вью
